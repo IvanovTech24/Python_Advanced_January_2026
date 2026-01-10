6a600e179e8ad44c1be61a492b8d2bfffc756596
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-10 13:55:59 +0200

    Added solutions for three more exercises, supermarket, water_dispenser and hot_potato

---
+++ Lists_as_Stacks_and_Queues _Lab _06_01_2026/05_hot_potato.py
@@ -0,0 +1,9 @@
+from collections import deque
+kids = deque(input().split())
+step = int(input())
+
+while len(kids) > 1:
+    kids.rotate(-(step - 1))
+    print(f"Removed {kids.popleft()}")
+
+print(f"Last is {kids.pop()}")
