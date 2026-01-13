48a900c4900195f1b14c4e1d42b60c5ee4b5973d
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-13 22:13:46 +0200

    Add another solution (with function), for finding the longest intersection

---
+++ 04_Tuples_and_Sets_Exercise_12_01_2026/05_longest_intersection_solution_2.py
@@ -0,0 +1,17 @@
+def create_set_from_range(range_str):
+    start, end = range_str.split(",")
+    return set(range(int(start), int(end) + 1))
+
+longest_intersection = set()
+
+for _ in range(int(input())):
+    first_range, second_range = input().split("-")
+    first_set = create_set_from_range(first_range)
+    second_set = create_set_from_range(second_range)
+
+    current_intersection = first_set.intersection(second_set)
+
+    if len(current_intersection) > len(longest_intersection):
+        longest_intersection = current_intersection
+
+print(f"Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}")
