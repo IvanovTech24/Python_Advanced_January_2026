4172c404b488342f8373e94553b7ca05d880cbb2
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-29 11:43:17 +0200

    Add function to retrieve and display personal information

---
+++ 09_Functions_Advanced_Lab_20_01_2026/02_person_info.py
@@ -0,0 +1,9 @@
+def get_info(name, age, town):
+    return f"This is {name} from {town} and he is {age} years old"
+
+my_dict = {
+    "name": "George",
+    "town": "Sofia",
+    "age": 20
+}
+print(get_info(**my_dict))
