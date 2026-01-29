9df26018bf3b6683e6e55ab778d8a8daa7935389
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-29 13:02:07 +0200

    Add nested function to calculate factorial of a number

---
+++ 09_Functions_Advanced_Lab_20_01_2026/00_demo.py
@@ -0,0 +1,12 @@
+# nested functions
+def factorial(number):
+    if not isinstance(number, int) or number < 0:
+        return f"Sorry, 'number' is incorrect"
+    def inner_factorial(n):
+        fact = 1
+        for i in range(1, n + 1):
+            fact *= i
+        return fact
+    return inner_factorial(number)
+
+print(factorial(3))
