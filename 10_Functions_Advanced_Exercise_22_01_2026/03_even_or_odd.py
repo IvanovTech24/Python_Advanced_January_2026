c9d148383cdb9554a6724ea250f80a9f0f97b425
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-30 22:33:36 +0200

    Add function to filter even and odd numbers using filter() and list comprehension

---
+++ 10_Functions_Advanced_Exercise_22_01_2026/03_even_or_odd.py
@@ -0,0 +1,17 @@
+#Solution with filter() method
+def even_odd(*args):
+    command = args[-1]
+    if command == "even":
+        return list(filter(lambda x: x % 2 == 0, args[:-1]))
+    return list(filter(lambda x: x % 2 != 0, args[:-1]))
+
+#Solution with list comprehension
+def even_odd(*args):
+    command = args[-1]
+    if command == "even":
+        return [x for x in args[:-1] if x % 2 == 0]
+    return [x for x in args[:-1] if x % 2 != 0]
+
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
