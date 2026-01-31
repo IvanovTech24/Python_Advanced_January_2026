b925089e2c48b9077a3f66c31f62d6c5a241c6c6
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-01-31 13:38:39 +0200

    Add age_assignment function with for loop and comprehension solutions

---
+++ 10_Functions_Advanced_Exercise_22_01_2026/08_age_assignment.py
@@ -0,0 +1,27 @@
+# Solution with for loop
+def age_assignment(*args, **kwargs):
+    persons = {}
+    for name in args:
+        persons[name] = kwargs[name[0]]
+
+    persons = sorted(persons.items())
+
+    result = []
+    for name, age in persons:
+        result.append(f"{name} is {age} years old.")
+
+    return "\n".join(result)
+
+# Solution with comprehension
+def age_assignment(*args, **kwargs):
+    persons = {}
+    for name in args:
+        persons[name] = kwargs[name[0]]
+
+    persons = sorted(persons.items())
+
+    return "\n".join(f"{name} is {age} years old." for name, age in persons)
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print()
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
