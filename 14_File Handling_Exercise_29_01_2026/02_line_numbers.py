a499521f75584cddfda30e9ea6e88744aca5ddc8
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-02-05 19:52:47 +0200

    Add file handling task using with statement for reading and creatin files

---
+++ 14_File Handling_Exercise_29_01_2026/02_line_numbers.py
@@ -0,0 +1,18 @@
+from string import punctuation
+
+
+with open("text.txt") as file, open("output.txt", "w") as output_file:
+    result = []
+    for row_index, line in enumerate(file, 1):
+        letter_count = 0
+        punctuation_count = 0
+
+        for char in line:
+            if char.isalpha():
+                letter_count += 1
+            elif char in punctuation:
+                punctuation_count += 1
+
+        result.append(f"Line {row_index}: {line.strip()} ({letter_count})({punctuation_count})")
+
+    output_file.write("\n".join(result))
