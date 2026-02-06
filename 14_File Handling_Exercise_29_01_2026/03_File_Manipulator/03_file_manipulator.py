82b25cfd843bfffd5304174478ef49f0be8a25e4
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-02-06 19:16:39 +0200

    Add file to an additional directory

---
+++ 14_File Handling_Exercise_29_01_2026/03_File_Manipulator/03_file_manipulator.py
@@ -0,0 +1,28 @@
+import os
+
+while True:
+    line = input()
+    if line == "End":
+        break
+
+    command, filename, *args = line.split("-")
+
+    if command == "Create":
+        open(filename, "w").close()
+    elif command == "Add":
+        with open(filename, "a") as file:
+            file.write(f"{args[0]}\n")
+    elif command == "Replace":
+        try:
+            with open(filename, "r+") as file:
+                content = file.read()
+                file.seek(0)
+                file.truncate(0)
+                file.write(content.replace(args[0], args[1]))
+        except FileNotFoundError:
+            print("An error occurred")
+    elif command == "Delete":
+        if os.path.exists(filename):
+            os.remove(filename)
+        else:
+            print("An error occurred")
