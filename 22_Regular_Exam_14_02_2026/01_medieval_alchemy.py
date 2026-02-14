3b2af4b5b513bc986952c279daa0deffa8382608
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-02-14 13:42:14 +0200

    feat: alchemy game logic

---
+++ 22_Regular_Exam_14_02_2026/01_medieval_alchemy.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+potion_names = [
+    ("Brew of Immortality", 110),
+    ("Essence of Resilience", 100),
+    ("Draught of Wisdom", 90),
+    ("Potion of Agility", 80),
+    ("Elixir of Strength", 70)
+]
+
+
+substances = [int(x) for x in input().split(", ")]
+crystals = deque(int(x) for x in input().split(", "))
+
+crafted_potions = []
+
+while substances and crystals:
+
+    if len(crafted_potions) == 5:
+        break
+
+    substance = substances.pop()
+    crystal = crystals.popleft()
+    current_sum = substance + crystal
+
+    found = False
+    for name, energy in potion_names:
+        if energy == current_sum and name not in crafted_potions:
+            crafted_potions.append(name)
+            found = True
+            break
+
+    if found:
+        continue
+
+    possible = [energy for name, energy in potion_names if energy < current_sum and name not in crafted_potions]
+
+    if possible:
+        best_energy = max(possible)
+        for name, energy in potion_names:
+            if energy == best_energy:
+                crafted_potions.append(name)
+                break
+
+        new_crystal_energy = crystal - 20
+        if new_crystal_energy > 0:
+            crystals.append(new_crystal_energy)
+    else:
+        new_crystal_energy = crystal - 5
+        if new_crystal_energy > 0:
+            crystals.append(new_crystal_energy)
+
+if len(crafted_potions) == 5:
+    print("Success! The alchemist has forged all potions!")
+else:
+    print("The alchemist failed to complete his quest.")
+
+if crafted_potions:
+    print(f"Crafted potions: {', '.join(crafted_potions)}")
+
+if substances:
+    print(f"Substances: {', '.join(str(x) for x in reversed(substances))}")
+
+if crystals:
+    print(f"Crystals: {', '.join(str(x) for x in crystals)}")
